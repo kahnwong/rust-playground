31b3764f03e00f2c599a03af53d52e12bbe1f07f
Karn Wong <karn@karnwong.me>
2024-09-10 17:02:50 +0700

    add group by

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,14 @@ use std::fs::File;
 
 pub fn calculate() -> Result<DataFrame, PolarsError> {
     let file = File::open("data/sample.csv")?;
-    let df = CsvReader::new(file).finish()?.lazy().collect()?;
-    Ok(df)
+    let df = CsvReader::new(file).finish()?.lazy();
+
+    let df_transformed = df
+        .select(&[col("foo"), col("bar")])
+        .group_by_stable([col("foo")])
+        .agg([col("bar").min().alias("min_bar")])
+        .collect()?;
+    Ok(df_transformed)
 }
 
 fn main() {
